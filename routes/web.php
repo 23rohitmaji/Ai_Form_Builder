c10627611da37717dbeb34516792c76eaa14436c
Rohit Maji <72294808+23rohitmaji@users.noreply.github.com>
2026-08-06 00:15:34 +0530

    fix: make Vercel API routes stateless

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,12 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FormAnalyticsController;
 use App\Http\Controllers\Api\FormController;
 use App\Http\Controllers\Api\FormSubmissionController;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,7 +20,13 @@ Route::get('/f/{slug}', function () {
     return view('welcome');
 });
 
-Route::prefix('xapi')->as('xapi.')->group(function () {
+Route::prefix('xapi')->as('xapi.')->withoutMiddleware([
+    EncryptCookies::class,
+    AddQueuedCookiesToResponse::class,
+    StartSession::class,
+    ShareErrorsFromSession::class,
+    PreventRequestForgery::class,
+])->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 
